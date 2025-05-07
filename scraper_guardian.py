40b482101dd4d4a92ced1e919784f61efa899dbb
Dan Yu <dan.yu.h97@gmail.com>
2025-05-07 18:33:45 +0200

    Create first relational databases for 'authors' and 'books'

--- scraper_guardian.py
+++ scraper_guardian.py
@@ -2,6 +2,7 @@ from download_webpage import simple_get
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+from cs50 import SQL
 
 # Download HTML from URL 
 GUARDIAN_URL = "https://www.theguardian.com/books/2015/aug/17/the-100-best-novels-written-in-english-the-full-list"
@@ -16,7 +17,7 @@ authors = []
 for i in range(len(all_p)):
     if i % 2 ==0:
         text = all_p[i].text
-        match_title = re.match(r"^(.*)(?=\bby\b)", text)
+        match_title = re.match(r'\d+\.\s*(.*?)(?=\bby\b(?!.*\bby\b))', text)
         match_author = re.findall(r'\bby\b\s*(.*?)(?:\s*\(\d{4}\))?\s*$', text)
         titles.append(match_title.group(1))
         authors.append(match_author[-1])
@@ -24,6 +25,13 @@ for i in range(len(all_p)):
 # Create relational databases
 open("books.db", "w").close()
 db = SQL("sqlite:///books.db")
-db.execute("CREATE TABLE authors (id INTEGER, name TEXT, gender smallint, PRIMARY KEY(id))")
+db.execute("CREATE TABLE authors (id INTEGER, name TEXT, PRIMARY KEY(id))")
 db.execute("CREATE TABLE books (book_id INTEGER, title TEXT, PRIMARY KEY(book_id))")
 
+for title in titles:
+    print(title)
+    title = title.strip().upper()
+    id  = db.execute("INSERT INTO books (title) VALUES(?)", title)
+for author in authors:
+    author = author.strip().upper()
+    id = db.execute("INSERT INTO authors (name) VALUES(?)", author)
